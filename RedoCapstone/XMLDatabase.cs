1d850396cbc1332ade8ec0734119f45bd75aaabd
RichmondVang <Vangrich741@gmail.com>
2025-04-21 22:30:02 -0500

    updating previous code

--- RedoCapstone/XMLDatabase.cs
+++ RedoCapstone/XMLDatabase.cs
@@ -11,15 +11,13 @@ class XMLDatabase{
         Console.Write("Enter your Character Name: ");
         string charName = Console.ReadLine();
         Console.Write("Enter Class: ");
-        string class = Console.ReadLine();
+        string cls = Console.ReadLine();
         Console.Write("Enter level: ");
         int level = int(Console.ReadLine());
         Console.Write("Enter Race: ");
         string class = Console.ReadLine();
 
         // this is just going to be a basic stat point distribution. Can make it more advanced later
-        while(statPoints > 0){
-            Console.Write("Allocate stat points (27 stat points available)");
             Console.Write("Strength (base = 8): ");
             int str = int(Console.ReadLine());
             Console.Write("Dexterity (base = 8): ");
@@ -32,10 +30,26 @@ class XMLDatabase{
             int wis = int(Console.ReadLine());
             Console.Write("Charisma (base = 8): ");
             int charisma = int(Console.ReadLine());
-            
-        }
+
         XElement charactersXML = new XElement("Characters",
             new XElement("Character",
-                new Xelement("Character Name", )))
+                new Xelement("Character Name", charName),
+                new Xelement("Class", cls),
+                new Xelement("Level", level),
+                new Xelement("Race", race),
+                new Xelement("Strength", str),
+                new Xelement("Dexterity", dex),
+                new Xelement("Constitution", cons),
+                new Xelement("Intelligence", intel),
+                new Xelement("Wisdom", wis),
+                new Xelement("Charisma"), charisma)
+        );
+        // Save to Database
+        charactersXML.Save("characters.xml");
+        Console.WriteLine("XML database saved as characters.xml");
+
+        // Reading XML Database
+        XDocument doc = XDocument.Load"(characters.xml");
+
     }
 }
