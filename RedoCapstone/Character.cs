eea8fdf2689d17dd96cdbbca7d286bc7defec67d
Averydiederich0 <Averydiederich0@gmail.com>
2025-04-21 19:11:47 -0500

    Creating a new branch so our Master branch dosnt become curropt

---
+++ RedoCapstone/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoCapstone
+{
+    internal class Character : Entity
+    {
+
+        string name;
+        string race;
+        int dex;
+        int con;
+        int wil;
+
+        Character(string name, string race, int dex, int con, int wil)
+        {
+            this.name = name;
+            this.race = race;
+            this.dex = dex;
+            this.con = con;
+            this.wil = wil;
+        }
+    }
+}
---
+++ RedoCapstone/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Dont worry about this
+namespace RedoCapstone
+{
+    public class User
+    {
+        string username;
+    }
+}
