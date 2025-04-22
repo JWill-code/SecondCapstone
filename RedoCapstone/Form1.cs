c4b6bf34c4056cdb59f46c1382898712977602b9
JWill-code <willjg24@uwgb.edu>
2025-04-21 22:13:32 -0500

    minor gui creation: made two

--- RedoCapstone/Form1.cs
+++ RedoCapstone/Form1.cs
@@ -12,9 +12,30 @@ namespace RedoCapstone
 {
     public partial class Form1 : Form
     {
+        List<string> name = new List<string>();
+        List<string> species = new List<string>();
+        public static Form1 instance;
         public Form1()
         {
             InitializeComponent();
+            instance = this; //when created, save instance
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //switches tabs
+            CharacterCreate form = new CharacterCreate();
+            form.Show();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
